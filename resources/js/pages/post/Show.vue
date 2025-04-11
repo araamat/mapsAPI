0f05e3587576bea04343e05087c6c73e3e49bc52
araamat <raamatandrus@gmail.com>
2025-04-11 10:48:56 +0300

    first commit

---
+++ resources/js/pages/post/Show.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue';
+import Textarea from '@/components/ui/textarea/Textarea.vue';
+import AppLayout from '@/layouts/AppLayout.vue';
+import { BreadcrumbItem } from '@/types';
+
+defineProps(['post'])
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Show post',
+        href: '/posts/show',
+    },
+];
+</script>
+
+<template>
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="my-12 mx-auto w-full max-w-2xl">
+           <div>
+             <h1 class="text-2xl font-semibold tracking wide">{{ post.title }}</h1>
+             <p class="">{{ post.description }}</p>
+            <!-- {{ post }} -->
+           </div>
+           <div>
+                <form @submit.prevent="submit">
+                    <div class="relative">
+                        <Textarea class="h-full w-full"></Textarea>
+                        <Button class="absolute bottom-4 right-4 z-10">Submit</Button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </AppLayout>
+</template>
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MarkerController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::get('dashboard', DashboardController::class)
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route:: resource('posts', PostController::class)
+    ->middleware(['auth', 'verified'])
+    ->names([
+        'index' => 'posts.index',
+        'create' => 'posts.create',
+        'store' => 'posts.store',
+        'show' => 'posts.show',
+        'edit' => 'posts.edit',
+        'update' => 'posts.update',
+        'destroy' => 'posts.destroy',
+    ]);
+
+ Route::resource('markers', MarkerController::class)
+    ->middleware(['auth', 'verified'])
+    ->names([
+        'index' => 'markers.index',
+        'create' => 'markers.create',
+        'store' => 'markers.store',
+        'show' => 'markers.show',
+        'edit' => 'markers.edit',
+        'update' => 'markers.update',
+        'destroy' => 'markers.destroy',
+    ]);
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
