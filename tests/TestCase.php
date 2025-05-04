666aee1665c32257d3e5aea4743e0e6b4ed02041
araamat <raamatandrus@gmail.com>
2025-05-04 21:40:52 +0300

    kas nüüd paraneb

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,13 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tests\CreatesApplication as TestsCreatesApplication;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use TestsCreatesApplication, RefreshDatabase;
 }
