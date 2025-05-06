f01186daeddeac08bea87822252147e52c045898
araamat <raamatandrus@gmail.com>
2025-05-06 14:41:48 +0300

    vana tagasi

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,30 @@
 import vue from '@vitejs/plugin-vue';
 import autoprefixer from 'autoprefixer';
 import laravel from 'laravel-vite-plugin';
-import { resolve } from 'node:path';
 import path from 'path';
 import tailwindcss from 'tailwindcss';
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/build/', // ✅ tagab õiged suhtelised teed
-
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
-            refresh: true,
-        }),
-        vue(),
-    ],
-
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './resources/js'),
-            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
-        },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.ts'],
+      ssr: 'resources/js/ssr.ts',
+      refresh: true,
+    }),
+    vue(), // ⬅️ Vajalik, et .vue failid töötaksid
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './resources/js'),
+      'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
     },
-
-    css: {
-        postcss: {
-            plugins: [tailwindcss, autoprefixer],
-        },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
     },
-
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-    },
+  },
+  
 });
-
